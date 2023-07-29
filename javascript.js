a756e81119a050abcc896a200e19d8e91f90da38
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2023-07-29 11:03:38 +0600

    Javascript Problems 04

---
+++ javascript.js
@@ -0,0 +1,26 @@
+// Javascript Problem 01
+var harrysMom=1000;
+var orangePrice=350;
+var applePrice=250;
+var totalCost=orangePrice + applePrice;
+var shopkeeperReturn=harrysMom-totalCost;
+console.log(shopkeeperReturn);
+// Javascript Problem 02
+var mathmatics=75.25;
+var biology=65;
+var chemistry=80;
+var physics=35.45;
+var bangla=99.50;
+var totalNumber=(mathmatics+biology+chemistry+physics+bangla);
+var averageNumber=(totalNumber/5);
+averageNumber=averageNumber.toFixed(2);
+console.log(averageNumber);
+// Javascript Problem 03
+var jhonsTeacherSays1="I am going to be";
+var jhonsTeacherSays2="an awesome web developer";
+console.log(jhonsTeacherSays1+jhonsTeacherSays2);
+// Javascript Problem 04
+var number=119;
+var divided=5;
+var remainder=number%divided;
+console.log(remainder);
